c15c968866d5bb357dc6cd67d23651dd82e0ee5d
Sorethea OK <sorethea@gmail.com>
2023-01-18 14:26:49 +0700

    first commit

--- config/app.php
+++ config/app.php
@@ -194,6 +194,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        \Modules\Test\Providers\TestServiceProvider::class,
 
     ],
 
