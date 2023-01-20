457781683f011e9cfad24cebae741837fc6a947f
Sorethea OK <sorethea@gmail.com>
2023-01-20 11:01:15 +0700

    first commit

--- Modules/Test/Providers/FilamentServiceProvider.php
+++ Modules/Test/Providers/FilamentServiceProvider.php
@@ -5,6 +5,7 @@ namespace Modules\Test\Providers;
 use Filament\Facades\Filament;
 use Filament\Navigation\NavigationGroup;
 use Filament\PluginServiceProvider;
+use Livewire\Livewire;
 use Modules\Test\Filament\Resources\RoleResource;
 use Spatie\LaravelPackageTools\Package;
 use Modules\Test\Filament\Pages\TestPage;
@@ -38,6 +39,8 @@ class FilamentServiceProvider extends PluginServiceProvider
 
     public function boot():void
     {
+        Livewire::component("CreateRole",RoleResource\Pages\CreateRole::class);
+        Livewire::component("EditRole",RoleResource\Pages\CreateRole::class);
         Filament::serving(function (){
             if(config('test.navigation-group.enabled'))
             Filament::registerNavigationGroups([
